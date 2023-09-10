0b4606aca308878cac7c33432e6bbcfb1d8a64f4
akayj <askingyj@gmail.com>
2023-09-10 21:27:01 +0800

    format

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -11,25 +11,25 @@ pb_high_power_fg="blue"
 pb_no_show=100
 
 function show_touchpad_battery() {
-	# 获取蓝牙触摸板的电量百分比
-	local battery_percent=$(ioreg -l 2>/dev/null | grep BatteryPercent | awk '{print $NF}')
+  # 获取蓝牙触摸板的电量百分比
+  local battery_percent=$(ioreg -l 2>/dev/null | grep BatteryPercent | awk '{print $NF}')
 
-	if [ -z "${battery_percent}" ]; then
-		return 1
-	fi
+  if [ -z "${battery_percent}" ]; then
+    return 1
+  fi
 
-	if ((battery_percent < ${pb_no_show})); then
-		if ((battery_percent < 50)); then
-			if ((battery_percent <= 30)); then
-				# 在status-right配置的末尾添加电量信息
-				echo "#[fg=${pb_low_power_fg}]${pb_prefix}${battery_percent}${pb_suffix}"
-			else
-				echo "#[fg=${pb_middle_power_fg}]${pb_prefix}${battery_percent}${pb_suffix}"
-			fi
-		else
-			echo "#[fg=${pb_high_power_fg}]${pb_prefix}${battery_percent}${pb_suffix}"
-		fi
-	fi
+  if ((battery_percent < ${pb_no_show})); then
+    if ((battery_percent < 50)); then
+      if ((battery_percent <= 30)); then
+        # 在status-right配置的末尾添加电量信息
+        echo "#[fg=${pb_low_power_fg}]${pb_prefix}${battery_percent}${pb_suffix}"
+      else
+        echo "#[fg=${pb_middle_power_fg}]${pb_prefix}${battery_percent}${pb_suffix}"
+      fi
+    else
+      echo "#[fg=${pb_high_power_fg}]${pb_prefix}${battery_percent}${pb_suffix}"
+    fi
+  fi
 }
 
 show_touchpad_battery
--- tmux_touchpad_battery.tmux
+++ tmux_touchpad_battery.tmux
@@ -8,31 +8,31 @@ readonly SCRITPS_DIR="${CURRENT_DIR}/scripts"
 source "${SCRITPS_DIR}/helpers.sh"
 
 placeholders=(
-	"\#{touchpad_battery}"
+  "\#{touchpad_battery}"
 )
 
 commands=(
-	"#($SCRITPS_DIR/battery.sh)"
+  "#($SCRITPS_DIR/battery.sh)"
 )
 
 do_interpolation() {
-	local all_interpolated="$1"
-	for ((i = 0; i < ${#commands[@]}; i++)); do
-		all_interpolated=${all_interpolated//${placeholders[$i]}/${commands[$i]}}
-	done
-	echo "$all_interpolated"
+  local all_interpolated="$1"
+  for ((i = 0; i < ${#commands[@]}; i++)); do
+    all_interpolated=${all_interpolated//${placeholders[$i]}/${commands[$i]}}
+  done
+  echo "$all_interpolated"
 }
 
 update_tmux_option() {
-	local option="$1"
-	local option_value="$(get_tmux_option "$option")"
-	local new_option_value="$(do_interpolation "$option_value")"
-	set_tmux_option "$option" "$new_option_value"
+  local option="$1"
+  local option_value="$(get_tmux_option "$option")"
+  local new_option_value="$(do_interpolation "$option_value")"
+  set_tmux_option "$option" "$new_option_value"
 }
 
 main() {
-	update_tmux_option "status-right"
-	update_tmux_option "status-left"
+  update_tmux_option "status-right"
+  update_tmux_option "status-left"
 }
 
 main
